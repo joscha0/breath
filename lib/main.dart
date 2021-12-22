2879363f068fd687eeee243984b4ec3cc8931351
Joscha von Andrian <joschavonandrian@gmail.com>
2021-12-22 19:42:00 +0100

    add style

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ import 'package:get/get.dart';
 
 void main() {
   runApp(GetMaterialApp(
+    theme: ThemeData(
+      scaffoldBackgroundColor: Colors.lightBlue[200],
+      textTheme: const TextTheme(
+        headline4: TextStyle(fontWeight: FontWeight.w300, color: Colors.white),
+      ),
+    ),
     debugShowCheckedModeBanner: false,
     home: HomePage(),
   ));
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:breath/modules/settings/settings_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:rive/rive.dart';
 
 import 'home_controller.dart';
@@ -10,17 +13,49 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue[200],
-      body: const Center(
-        child: RiveAnimation.asset('assets/lung.riv'),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-      floatingActionButton: FloatingActionButton(
+      appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        foregroundColor: Colors.white,
-        child: const Icon(Icons.settings),
-        onPressed: () {},
+        actions: [
+          IconButton(
+              onPressed: () {
+                Get.to(SettingsPage(),
+                    transition: Transition.rightToLeftWithFade);
+              },
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Text(
+              '5:00',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            width: double.infinity,
+            alignment: Alignment.center,
+          ),
+          CircularPercentIndicator(
+            radius: 280,
+            center: const Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 32, 32),
+              child: RiveAnimation.asset('assets/lung.riv'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: LinearPercentIndicator(),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              'breathe in',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          )
+        ],
       ),
     );
   }
